53cb737dd0689978bb63c9db6b3cc57344fe62a0
Borislav Hadzhiev <opmftw@gmail.com>
2022-04-14 13:50:54 +0300

    update to cdk v2

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,7 +1,7 @@
-import * as ec2 from '@aws-cdk/aws-ec2';
-import * as lambda from '@aws-cdk/aws-lambda';
-import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
-import * as cdk from '@aws-cdk/core';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as cdk from 'aws-cdk-lib';
 import * as path from 'path';
 
 export class MyCdkStack extends cdk.Stack {
